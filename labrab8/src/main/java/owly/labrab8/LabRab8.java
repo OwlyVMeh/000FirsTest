86336a47fbc5047bd31b62efa963d00751c551b0
zhus <zhukov.sv@npt.ru>
2026-03-19 16:16:30 +0300

    тест второго пользователя

--- labrab8/src/main/java/owly/labrab8/LabRab8.java
+++ labrab8/src/main/java/owly/labrab8/LabRab8.java
@@ -3,6 +3,6 @@ package owly.labrab8;
 public class LabRab8 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World from zh!");
     }
 }
